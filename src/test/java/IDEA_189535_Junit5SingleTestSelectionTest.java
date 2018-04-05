d63d99b58bf985c2aa3592cc9f59624df1e166a0
nikowitt <github@wittenbeck-it.de>
2018-04-05 12:11:23 +0200

    replicated issue for IDEA-189535

--- src/test/java/IDEA_189535_Junit5SingleTestSelectionTest.java
+++ src/test/java/IDEA_189535_Junit5SingleTestSelectionTest.java
@@ -2,38 +2,29 @@ import static org.junit.jupiter.params.provider.Arguments.of;
 
 import java.util.stream.Stream;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.tngtech.jgiven.Stage;
-import com.tngtech.jgiven.junit5.SimpleScenarioTest;
 
-public class IDEA_189535_Junit5SingleTestSelectionTest extends SimpleScenarioTest<IDEA_189535_Junit5SingleTestSelectionTest.MyStage> {
+public class IDEA_189535_Junit5SingleTestSelectionTest {
 
 	private static Logger LOGGER = LoggerFactory.getLogger(IDEA_189535_Junit5SingleTestSelectionTest.class);
 
 	public static Stream<Arguments> test() {
 		return Stream.of(
-				of("a"),
-				of("b"),
-				of("c")
+				of("a","b"),
+				of("b","c"),
+				of("c","d")
 		);
 	}
 
 	@ParameterizedTest
 	@MethodSource
-	public void test(String arg) throws Exception {
-		given().$("a test: "+ arg, MyStage::doNothing);
+	public void test(String arg, String arg1) throws Exception {
+		LOGGER.error("Single test selection breaks when the test signature changes without wiping all existing test configurations.");
 	}
 	
-	static class MyStage extends Stage<MyStage> {
-
-		public MyStage doNothing() {
-			return self();
-		}
-	}
 }
